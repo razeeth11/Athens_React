d0328f2cf88e1a69fb87c4b4f104b6e710e5631c
razeeth11 <razeeth011@gmail.com>
2023-09-07 12:15:07 +0530

    Nav & Main

---
+++ src/NavBar.jsx
@@ -0,0 +1,33 @@
+export function NavBar() {
+  return (
+    <div className="flex justify-between items-center px-[50px] py-[20px]">
+      <img src="src\images\logo main.svg" alt="logo.svg" width="150px" />
+      <ul className="flex items-center gap-[40px] font-medium">
+        <li>
+          <a href="#">Features</a>
+        </li>
+        <li>
+          <a href="#">Demos</a>
+        </li>
+        <li>
+          <a href="#">Mobile-First</a>
+        </li>
+        <li>
+          <a href="#">Performance</a>
+        </li>
+        <li>
+          <a href="#">SEO</a>
+        </li>
+        <li>
+          <a href="#">Support</a>
+        </li>
+        <li>
+          <a href="#">Open Store Free</a>
+        </li>
+      </ul>
+      <button className="px-[30px] py-[12px] font-medium text-white bg-[#1c4a3e] rounded">
+        Purchase Now
+      </button>
+    </div>
+  );
+}
